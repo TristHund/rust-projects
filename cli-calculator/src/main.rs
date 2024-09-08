f9e0f78dccb085d0326ac86055c79effc88b1433
TristHund <TristanHundley@protonmail.com>
2024-09-08 23:05:24 +0200

    regex update

--- cli-calculator/src/main.rs
+++ cli-calculator/src/main.rs
@@ -1,16 +1,23 @@
+use regex::Regex;
 use std::io;
 
 fn main() {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
 
-    let tokens: Vec<&str> = input.trim().split_whitespace().collect();
+    let re = Regex::new(r"(\d+\.?\d*)\s*([\+\-\*/])\s*(\d+\.?\d*)").unwrap();
 
-    let num1: f64 = tokens[0].parse().expect("Not a valid number");
-    let operator = tokens[1];
-    let num2: f64 = tokens[2].parse().expect("Not a valid number");
+    if let Some(captures) = re.captures(&input) {
+        let num1: f64 = captures[1].parse().unwrap();
+        let operator = &captures[2];
+        let num2: f64 = captures[3].parse().unwrap();
 
-    println!("{}", calculate(operator, num1, num2)); 
+        println!("{}", calculate(operator, num1, num2));
+    } else {
+        println!("Invalid input.");
+    }
 }
 
 fn calculate(op: &str, num1: f64, num2: f64) -> f64 {
@@ -22,4 +29,3 @@ fn calculate(op: &str, num1: f64, num2: f64) -> f64 {
         _ => panic!("Unknown operator"),
     }
 }
-
